f93e807d15115ab93e8f25fd9f7a204c444d0e43
Akshat Harlalka <akshatharlalka.ah@icloud.com>
2026-05-16 14:34:16 +0530

    game created creating boost button

--- game/food.py
+++ game/food.py
@@ -3,11 +3,31 @@ import pygame
 
 
 class Food:
-    def __init__(self, config):
+    def __init__(
+        self,
+        config,
+        x=None,
+        y=None,
+    ):
         self.config = config
 
-        self.x = random.randint(0, config.WIDTH)
-        self.y = random.randint(0, config.HEIGHT)
+        self.x = (
+            x
+            if x is not None
+            else random.randint(
+                0,
+                config.WIDTH
+            )
+        )
+
+        self.y = (
+            y
+            if y is not None
+            else random.randint(
+                0,
+                config.HEIGHT
+            )
+        )
 
         self.color = (
             random.randint(100, 255),
@@ -15,10 +35,16 @@ class Food:
             random.randint(100, 255),
         )
 
+    # =========================
+    # DRAW
+    # =========================
     def draw(self, screen):
         pygame.draw.circle(
             screen,
             self.color,
-            (int(self.x), int(self.y)),
+            (
+                int(self.x),
+                int(self.y),
+            ),
             self.config.FOOD_RADIUS,
         )
